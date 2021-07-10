12988ff8c25a248e4a63f5250c5e6c780ae41bae
1102jhc <73589005+1102jhc@users.noreply.github.com>
2021-07-10 12:38:44 +0900

    upload pickle.py

---
+++ my_pickle.py
@@ -0,0 +1,16 @@
+# pickle, 프로그램상에서 사용하는 데이터를 파일 형태로 저장
+# 항상 바이너리 파일 사용
+
+import pickle
+
+# profile_file = open("profile.pickle", "wb")
+# profile = {"이름":"박명수", "나이":30, "취미":["축구","골프","코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) 
+# profile_file.close()
+
+
+profile_file = open("profile.pickle", "rb")
+profile = pickle.load(profile_file) # file에 있는 정보를 profile에 불러오기
+print(profile)
+profile_file.close()
