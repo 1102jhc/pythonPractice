b1353b80cf9176c0bbe5eb819fe1f96258216b42
1102jhc <73589005+1102jhc@users.noreply.github.com>
2021-07-11 10:56:37 +0900

    add 멤버변수.py

--- my_class.py
+++ my_class.py
@@ -38,8 +38,22 @@ class Unit:
         print("{0} 유닛이 생성되었습니다.".format(self.name))
         print("체력 {0}, 공격력 {1}\n".format(self.hp, self.damage))
 
-marine1 = Unit("마린", 40, 5)
-marine2 = Unit("마린", 40, 5)
-tank = Unit("탱크", 150, 35)
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
 
 # __init__ 파이썬에서 사용하는 생성자
+
+# 레이스 : 공중 유닛, 비행기, 클로킹 (상대방에게 보이지 않음)
+wraith1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True
+
+if wraith2.clocking == True:
+    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+
+# 파이썬은 객체의 변수를 외부에서 추가로 만들 수 있음
