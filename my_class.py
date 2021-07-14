1997262587870bbfe20ddf54543903a22ba17b4f
Hee <1102jhc@gmail.com>
2021-07-14 09:56:12 +0900

    update my_class.py

--- my_class.py
+++ my_class.py
@@ -138,71 +138,138 @@
 
 #==================================================================================================
 
-# 일반 유닛
-class Unit:
-    def __init__(self, name, hp, speed):
-        self.name = name
-        self.hp = hp
-        self.speed = speed
+# # 일반 유닛
+# class Unit:
+#     def __init__(self, name, hp, speed):
+#         self.name = name
+#         self.hp = hp
+#         self.speed = speed
 
-    def move(self, location):
-        print("[지상 유닛 이동]")
-        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]" \
-            .format(self.name, location, self.speed))
+#     def move(self, location):
+#         print("[지상 유닛 이동]")
+#         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]" \
+#             .format(self.name, location, self.speed))
 
-# 공격 유닛
-class AttackUnit(Unit):
-    def __init__(self, name, hp, speed, damage):
-        Unit.__init__(self, name, hp, speed)
-        self.damage = damage
+# # 공격 유닛
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, speed, damage):
+#         Unit.__init__(self, name, hp, speed)
+#         self.damage = damage
 
-    def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]" \
-            .format(self.name, location, self.damage))
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]" \
+#             .format(self.name, location, self.damage))
 
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
-        if self.hp <= 0:
-            print("{0} : 파괴되었습니다.".format(self.name))
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
 
-# 드랍쉽 : 공중 유닛, 수송기, 마린 / 파이어뱃 / 탱크 등을 수송 공격 X
-# 날 수 있는 기능을 가진 클래스
-class Flyable:
-    def __init__(self, flying_speed):
-        self.flying_speed = flying_speed
+# # 드랍쉽 : 공중 유닛, 수송기, 마린 / 파이어뱃 / 탱크 등을 수송 공격 X
+# # 날 수 있는 기능을 가진 클래스
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
 
-    def fly(self, name, location):
-        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_speed))
+#     def fly(self, name, location):
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_speed))
 
-# 공중 공격 유닛 클래스
-class FlyableAttackUnit(AttackUnit, Flyable):
-    def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, 0, damage)
-        Flyable.__init__(self, flying_speed)
+# # 공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, 0, damage)
+#         Flyable.__init__(self, flying_speed)
 
-    def move(self, location):
-        print("[공중 유닛 이동]")
-        self.fly(self.name, location)
+#     def move(self, location):
+#         print("[공중 유닛 이동]")
+#         self.fly(self.name, location)
 
-# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "3시")
+# # 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
 
-# 벌쳐 : 지상유닛, 기동성이 좋음
-vulture = AttackUnit("벌쳐", 80, 10, 20)
+# # 벌쳐 : 지상유닛, 기동성이 좋음
+# vulture = AttackUnit("벌쳐", 80, 10, 20)
 
-# 배틀크루저 : 공중 유닛, 체력 좋음, 공격력 좋음
-battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+# # 배틀크루저 : 공중 유닛, 체력 좋음, 공격력 좋음
+# battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
 
-vulture.move("11시")
-# battlecruiser.move(battlecruiser.name, "9시")
-battlecruiser.move("9시")
+# vulture.move("11시")
+# # battlecruiser.move(battlecruiser.name, "9시")
+# battlecruiser.move("9시")
  
  #==================================================================================================
 
- # 일반 유닛
+#  # 일반 유닛
+# class Unit:
+#     def __init__(self, name, hp, speed):
+#         self.name = name
+#         self.hp = hp
+#         self.speed = speed
+
+#     def move(self, location):
+#         print("[지상 유닛 이동]")
+#         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]" \
+#             .format(self.name, location, self.speed))
+
+# # 공격 유닛
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, speed, damage):
+#         Unit.__init__(self, name, hp, speed)
+#         self.damage = damage
+
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]" \
+#             .format(self.name, location, self.damage))
+
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+
+# # 드랍쉽 : 공중 유닛, 수송기, 마린 / 파이어뱃 / 탱크 등을 수송 공격 X
+# # 날 수 있는 기능을 가진 클래스
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
+
+#     def fly(self, name, location):
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_speed))
+
+# # 공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, 0, damage)
+#         Flyable.__init__(self, flying_speed)
+
+#     def move(self, location):
+#         print("[공중 유닛 이동]")
+#         self.fly(self.name, location)
+
+# # 건물
+# class BuildingUnit(Unit):
+#     def __init__(self, name, hp, location):
+#         pass    # 일단은 넘어가는 의미, 일단 완성된 것처럼 보임
+
+# # 서플라이 디폿 : 건물, 1개 건물 = 8 유닛.
+# supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
+
+# def game_start():
+#     print("[알림] 새로운 게임을 시작합니다.")
+
+# def game_over():
+#     pass
+
+# game_start()
+# game_over
+
+ #==================================================================================================
+
+  # 일반 유닛
 class Unit:
     def __init__(self, name, hp, speed):
         self.name = name
@@ -250,18 +317,10 @@ class FlyableAttackUnit(AttackUnit, Flyable):
         print("[공중 유닛 이동]")
         self.fly(self.name, location)
 
+# 건물
 class BuildingUnit(Unit):
     def __init__(self, name, hp, location):
-        pass    # 일단은 넘어가는 의미, 일단 완성된 것처럼 보임
-
-# 서플라이 디폿 : 건물, 1개 건물 = 8 유닛.
-supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
-
-def game_start():
-    print("[알림] 새로운 게임을 시작합니다.")
-
-def game_over():
-    pass
-
-game_start()
-game_over
+        # Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0)   # super() -> self 사용 x, 상속 할 때 부모 클래스의 초기화
+        self.location = location
+        
