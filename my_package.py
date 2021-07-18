a4025ddcf561a01990a380b5eeffe21e3742d8d7
heechulJung <heechul.dev@gmail.com>
2021-07-18 10:21:31 +0900

    add __all__.py

--- my_package.py
+++ my_package.py
@@ -7,7 +7,11 @@
 # trip_to = ThailandPackage()
 # trip_to.detail()
 
-from travel import vietnam
-trip_to = vietnam.VietnamPackage()
-trip_to.detail()
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
 
+from travel import *    # *을 쓴다는 것은 travel에 있는 모든 것을 가져오겠다는 것인데 실제로는 개발자가 공개 범위를 설정해줘야 한다.
+#trip_to = vietnam.VietnamPackage()
+trip_to = thailand.ThailandPackage()
+trip_to.detail()
