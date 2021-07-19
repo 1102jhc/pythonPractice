0f55e3fed8c1a4242d172e96529fbdbcd832c782
heechulJung.dev <heechulJung.dev@gmail.com>
2021-07-19 11:13:38 +0900

    add 패키지, 모듈위치.py

--- my_package.py
+++ my_package.py
@@ -12,6 +12,12 @@
 # trip_to.detail()
 
 from travel import *    # *을 쓴다는 것은 travel에 있는 모든 것을 가져오겠다는 것인데 실제로는 개발자가 공개 범위를 설정해줘야 한다.
-#trip_to = vietnam.VietnamPackage()
-trip_to = thailand.ThailandPackage()
-trip_to.detail()
+# #trip_to = vietnam.VietnamPackage()
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
+
+# 패키지, 모듈 위치 출력
+import inspect
+import random
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
